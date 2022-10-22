89b5d7ad141700eb12391463dc8b1647949e1e10
Damian Cabrera <108781459+DamianRCabrera@users.noreply.github.com>
2022-10-22 02:11:49 -0300

    Added feature to cart
    -Feature: add cart item to cart

--- public/js/shoppingCart.js
+++ public/js/shoppingCart.js
@@ -1,34 +1,70 @@
 class ShopCart {
-  async ajax(url, method = "get") {
-    return await fetch(url, { method: method }).then((res) => res.text());
+  constructor() {
+    this.url = "http://localhost:8080/api/cart/";
+    this.addedIds = new Set();
+    this.cartIDs = {
+      productsID: [],
+    };
   }
 
-  
+  async ajax(url, method, ids) {
+    return await fetch(url, { method: method, body: ids, headers: {
+      'Content-Type': 'application/json'} }).then(
+      (res) => res.text()
+    );
+  }
+
+  async loadProductToCart() {
+    console.log(JSON.stringify(this.cartIDs));
+    const container = document.querySelector(".shopping-cart-item-container");
+    let ids = JSON.stringify(this.cartIDs);
+    const viewContent = await this.ajax(this.url, "post", ids);
+    container.innerHTML = viewContent;
+  }
+
+  async addProductToCart(id) {
+    this.addedIds.add(id);
+    console.log("deberia agregar");
+
+    this.cartIDs.productsID.push({ "id": id, quantity: 1 });
+    await this.loadProductToCart();
+  }
+
+  init() {
+    const modalCheckbox = document.getElementById("main-nav-cart");
+    document.addEventListener("click", (e) => {
+      if (modalCheckbox.checked) {
+        if (e.target.className.includes("main-cart-toggle")) {
+          return;
+        } else if (e.target.className.includes("shopping-cart__close__image")) {
+          modalCheckbox.checked = false;
+          return;
+        } else if (!e.target.className.includes("shopping-cart")) {
+          modalCheckbox.checked = false;
+          return;
+        }
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      if (modalCheckbox.checked) {
+        if (event.key == "Escape") {
+          modalCheckbox.checked = false;
+          return;
+        }
+      }
+    });
+
+    document.addEventListener("click", (e) => {
+      if (e.target.className.includes("card__link-add")) {
+        e.preventDefault();
+        console.log("debug");
+        const id = Number(e.target.getAttribute("data-id"));
+        this.addProductToCart(id);
+      }
+    });
+  }
 }
 
-// Event Handler for modal of shooping-cart
-
-const modalCheckbox = document.getElementById("main-nav-cart");
-
-document.addEventListener("click", (e) => {
-  if (modalCheckbox.checked) {
-    if (e.target.className.includes("main-cart-toggle")) {
-      return;
-    } else if (e.target.className.includes("shopping-cart__close__image")) {
-      modalCheckbox.checked = false;
-      return;
-    } else if (!e.target.className.includes("shopping-cart")) {
-      modalCheckbox.checked = false;
-      return;
-    }
-  }
-});
-
-document.addEventListener("keyup", (e) => {
-  if (modalCheckbox.checked) {
-    if (e.key == "Escape") {
-      modalCheckbox.checked = false;
-      return;
-    }
-  }
-});
+const shopCart = new ShopCart();
+shopCart.init();
