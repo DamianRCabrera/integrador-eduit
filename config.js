91cec17372e4d70105fc5f1fa2f3ee35b4420c06
Damian Cabrera <108781459+DamianRCabrera@users.noreply.github.com>
2022-11-14 21:23:13 -0300

    Update config.js
    - Added process.env.port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const PERSISTENCE_TYPE = {
 };
 
 const config = {
-  PORT: 8080,
+  PORT: process.env.PORT || 8080,
   PERSISTENCE_TYPE: PERSISTENCE_TYPE.TYPE_MONGODB,
   MONGODB_CONNECTION_STR: 'mongodb+srv://admin:admin@e-commerce.wv4szyy.mongodb.net/ecommerce?retryWrites=true&w=majority',
   MONGODB_TIMEOUT: 2000,
